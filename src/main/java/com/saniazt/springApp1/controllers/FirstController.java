7daafcc7ef6f9f6ed29e88a9ae81c5ed9a2aec91
saniazt <saniazt@gmail.com>
2022-10-21 13:59:08 +0300

    21.10.2022 13:58

--- src/main/java/com/saniazt/springApp1/controllers/FirstController.java
+++ src/main/java/com/saniazt/springApp1/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.saniazt.springApp1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +15,30 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false)String surname){
+                            @RequestParam(value = "surname",required = false)String surname,
+                            Model model){
 
-        System.out.println("Hello," +name+" "+surname);
+       // System.out.println("Hello," +name+" "+surname);
+        model.addAttribute("message","Hello," +name+" "+surname);
 
         return "first/hello";
+    }
+    @GetMapping("/calculate")
+    public String calculatorPage( @RequestParam(value = "a",required = false) int a,
+                              @RequestParam(value = "b",required = false) int b,
+                              @RequestParam(value = "action",required = false) String action,
+                              Model model2){
+        double result;
+        switch (action) {
+            case ("multiplication"): {result=a*b;} break;
+            case ("addition"): {result=a+b;} break;
+            case ("subtraction"): {result=a-b;} break;
+            case ("division"): {result=a/(double)b;} break;
+            default: result=0;
+        }
+        model2.addAttribute("calculationOperation","Result is "+result);
+
+        return "first/calculate";
     }
 
     @GetMapping("/goodbye")
